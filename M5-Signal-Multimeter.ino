595f871589babae861fa5ea0eb3590175e9cef5d
laurentopia <laurentopia@users.noreply.github.com>
2018-04-29 00:55:00 -0700

    now with heatmap colors

--- M5-Signal-Multimeter.ino
+++ M5-Signal-Multimeter.ino
@@ -9,13 +9,14 @@
 // TODO: fix the weird retults, FFT access wrong?
 // TODO: fix the oscillo draw which is a bit on the sloooooooowwwwwwwww siiiiiiiiide
 
+#include <dummy.h>
 #include <M5Stack.h>
 #include <arduinoFFT.h>
 arduinoFFT FFT = arduinoFFT();
 
 /////////////////////////////////////////////////////////////////////////
 const unsigned int SAMPLES = 512;				  // Must be a power of 2;
-const unsigned long SAMPLING_FREQUENCY = 10000UL; // Hz, must be 40000 or less due to ADC conversion time. Determines maximum frequency that can be analysed by the FFT Fmax=sampleF/2.
+const unsigned long SAMPLING_FREQUENCY = 20000UL; // Hz, must be 40000 or less due to ADC conversion time. Determines maximum frequency that can be analysed by the FFT Fmax=sampleF/2.
 const unsigned int SIGNAL_AMPLITUDE = 200;		  // Depending on your audio source level, you may need to increase this value;
 unsigned int sampling_period_ms = 0;
 unsigned long long microseconds = 0;
@@ -26,7 +27,7 @@ double oldOscilloBuffer[SAMPLES] = { 0 };
 double vOldSample[SAMPLES] = { 0 };
 unsigned long long newTime = 0, oldTime = 0;
 /////////////////////////////////////////////////////////////////////////
-const unsigned int MAX_BAND_HEIGHT = 100;
+const unsigned int MAX_BAND_HEIGHT = 70;
 const unsigned int OSCILLO_Y = 200;
 float OSCILLO_YSCALE = 0.015f;
 float FFTDisplayScale = 0;
@@ -35,6 +36,32 @@ String majorPeakFrequency = "";
 String THD = "";
 unsigned long majorPeakTime = 0;
 String oldFFTDisplayScale = "";
+
+bool getHeatMapColor(float value, uint8_t *red, uint8_t *green, uint8_t *blue)
+{
+	const int NUM_COLORS = 3;
+	//static float color[NUM_COLORS][3] = { { 0,100,255 },{ 0,255,0 },{ 255,255,0 },{ 255,0,0 } };
+	static float color[NUM_COLORS][3] = { { 50,0,255 },{ 255,0,0 } ,{ 255,255,0 } };
+	// A static array of 4 colors:  (blue,   green,  yellow,  red) using {r,g,b} for each.
+
+	int idx1;        // |-- Our desired color will be between these two indexes in "color".
+	int idx2;        // |
+	float fractBetween = 0;  // Fraction between "idx1" and "idx2" where our value is.
+
+	if (value <= 0) { idx1 = idx2 = 0; }    // accounts for an input <=0
+	else if (value >= 1) { idx1 = idx2 = NUM_COLORS - 1; }    // accounts for an input >=0
+	else
+	{
+		value = value * (NUM_COLORS - 1);        // Will multiply value by 3.
+		idx1 = floor(value);                  // Our desired color will be after this index.
+		idx2 = idx1 + 1;                        // ... and before this index (inclusive).
+		fractBetween = value - float(idx1);    // Distance between the two indexes (0-1).
+	}
+
+	*red = (color[idx2][0] - color[idx1][0])*fractBetween + color[idx1][0];
+	*green = (color[idx2][1] - color[idx1][1])*fractBetween + color[idx1][1];
+	*blue = (color[idx2][2] - color[idx1][2])*fractBetween + color[idx1][2];
+}
 
 void displayBand(int band, int dsize, const String &string)
 {
@@ -45,7 +72,10 @@ void displayBand(int band, int dsize, const String &string)
 	}
 	dsize = min(dsize, MAX_BAND_HEIGHT);
 	M5.Lcd.drawFastVLine(band, 0, MAX_BAND_HEIGHT, BLACK);
-	M5.Lcd.drawFastVLine(band, MAX_BAND_HEIGHT - dsize, dsize, GREEN);
+	uint8_t r, g, b;
+	getHeatMapColor((float)dsize/(float) MAX_BAND_HEIGHT, &r, &g, &b);
+	uint16_t color = m5.Lcd.color565(r, g, b);
+	M5.Lcd.drawFastVLine(band, MAX_BAND_HEIGHT - dsize, dsize, color);
 }
 
 void Capture()
@@ -71,13 +101,13 @@ void DrawOscillo()
 	{
 		//if (i == 0)
 		//{
-		M5.Lcd.drawPixel(i, OSCILLO_Y - oldOscilloBuffer[i] * OSCILLO_YSCALE, BLACK); //clear
-		M5.Lcd.drawPixel(i, OSCILLO_Y - buffer[i] * OSCILLO_YSCALE, RED);
+		//M5.Lcd.drawPixel(i, OSCILLO_Y - oldOscilloBuffer[i] * OSCILLO_YSCALE, BLACK); //clear
+		//M5.Lcd.drawPixel(i, OSCILLO_Y - buffer[i] * OSCILLO_YSCALE, RED);
 		//}
 		//else
 		//{
-			//M5.Lcd.drawLine(i - 1, OSCILLO_Y - oldOscilloBuffer[i - 1] * OSCILLO_YSCALE, i, OSCILLO_Y - oldOscilloBuffer[i] * OSCILLO_YSCALE, BLACK); //clear
-			//M5.Lcd.drawLine(i - 1, OSCILLO_Y - buffer[i - 1] * OSCILLO_YSCALE, i, OSCILLO_Y - buffer[i] * OSCILLO_YSCALE, RED);
+			M5.Lcd.drawLine(i - 1, OSCILLO_Y - oldOscilloBuffer[i - 1] * OSCILLO_YSCALE, i, OSCILLO_Y - oldOscilloBuffer[i] * OSCILLO_YSCALE, BLACK); //clear
+			M5.Lcd.drawLine(i - 1, OSCILLO_Y - buffer[i - 1] * OSCILLO_YSCALE, i, OSCILLO_Y - buffer[i] * OSCILLO_YSCALE, RED);
 		//M5.Lcd.drawFastVLine (i, OSCILLO_Y - min(oldOscilloBuffer[i - 1], oldOscilloBuffer[i]) * OSCILLO_YSCALE, max(1,abs(oldOscilloBuffer[i]- oldOscilloBuffer[i-1])) * OSCILLO_YSCALE, BLACK);
 		//M5.Lcd.drawFastVLine(i, OSCILLO_Y - min(buffer[i - 1], buffer[i]) * OSCILLO_YSCALE, max(1,abs(buffer[i] - buffer[i - 1])) * OSCILLO_YSCALE, RED);
 		//}
@@ -190,6 +220,29 @@ void Synusoide_Task(void *parameter)
 	vTaskDelete(NULL);
 }
 
+void ledcAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax = 255)
+{
+	uint32_t duty = (8191 / valueMax) * min(value, valueMax);
+	ledcWrite(channel, duty);
+}
+
+// Make a PWM generator task on core 0
+// Signal generator pin 2
+void LedC_Task(void *parameter)
+{
+	int phase = 128;
+	int phaseStep = 5;
+	ledcSetup(0, 600, 13);
+	ledcAttachPin(2, 0);
+
+	for (;;)
+	{
+		ledcAnalogWrite(0, phase);
+		vTaskDelay(20 / portTICK_PERIOD_MS);
+	}
+	vTaskDelete(NULL);
+}
+
 void taskCapture(void *pvParameters)
 {
 	for (;;)
@@ -237,6 +290,8 @@ void setup()
 	//xTaskCreatePinnedToCore(TaskDraw, "draw FFT", 8192, NULL, 2, NULL, 0);
 	//xTaskCreatePinnedToCore(TaskDebug, "debug", 8192, NULL, 2, NULL, 0);
 	//xTaskCreatePinnedToCore(Synusoide_Task, "sin output on 26", 8192, NULL, 2, NULL, 0);
+	xTaskCreatePinnedToCore(LedC_Task, "square wave on 26", 8192, NULL, 2, NULL, 0);
+
 }
 
 long int oldMillis = 0;
